776f858562ad1d68684e42724d440f2935484417
thatsShirleyLee <2690233316@qq.com>
2025-09-08 02:24:18 +0800

    feat: update configs

---
+++ .stylelintrc.cjs
@@ -0,0 +1,8 @@
+module.exports = {
+  extends: ['stylelint-config-standard'],
+  rules: {
+    'color-hex-length': 'short',
+    'declaration-block-trailing-semicolon': 'always',
+    'no-empty-source': null,
+  },
+};
