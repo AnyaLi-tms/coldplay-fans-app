7a58ce7f6775410e2bc546f218d0e77a8d7c6923
thatsShirleyLee <2690233316@qq.com>
2025-09-08 02:32:34 +0800

    feat: delete and update some configs

--- .stylelintrc.cjs
+++ .stylelintrc.cjs
@@ -1,5 +1,6 @@
 module.exports = {
   extends: ['stylelint-config-standard'],
+  ignoreFiles: ['node_modules/**', 'dist/**', '*.min.css'], // <-- 代替 .stylelintignore
   rules: {
     'color-hex-length': 'short',
     'declaration-block-trailing-semicolon': 'always',
--- eslint.config.js
+++ eslint.config.js
@@ -1,14 +1,40 @@
-import js from "@eslint/js";
-import globals from "globals";
-import pluginReact from "eslint-plugin-react";
 import { defineConfig } from "eslint/config";
+import js from "@eslint/js";
+import pluginReact from "eslint-plugin-react";
+import globals from "globals";
 
 export default defineConfig([
   {
     files: ["**/*.{js,mjs,cjs,jsx}"],
+    ignores: ["dist", "node_modules", "*.min.js", "public"], // <-- 这里替代 .eslintignore
+    languageOptions: { globals: globals.browser },
     plugins: { js },
     extends: ["js/recommended"],
-    languageOptions: { globals: globals.browser },
   },
   pluginReact.configs.flat.recommended,
+  {
+    rules: {
+      "no-var": "error",
+      "no-multiple-empty-lines": ["warn", { max: 1 }],
+      "no-unexpected-multiline": "error",
+      "no-useless-escape": "off",
+      eqeqeq: ["error", "always"],
+      quotes: ["error", "single", { avoidEscape: true }],
+      "arrow-body-style": ["error", "as-needed"],
+      "prefer-const": "error",
+      "object-curly-spacing": ["error", "always"],
+      "array-bracket-spacing": ["error", "never"],
+      "comma-dangle": ["error", "always-multiline"],
+      "no-console": "warn",
+      "no-debugger": "error",
+      "react/prop-types": "off",
+      "react/react-in-jsx-scope": "off",
+      "react/jsx-uses-vars": "error",
+      "react-hooks/rules-of-hooks": "error",
+      "react-hooks/exhaustive-deps": "warn",
+    },
+    settings: {
+      react: { version: "detect" },
+    },
+  },
 ]);
