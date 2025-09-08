643a7426f9c29d2bf6fb021b116728c48805f69c
OliverDong <oliver.dong@oocl.com>
2025-09-08 13:34:42 +0800

    feat:修改App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 import './App.css';
 import Tabbar from './layout/Tabbar';
 import Main from './layout/Main';
-import Login from './pages/Home/Login';
 function App() {
   return (
     <>
       <Tabbar />
       <Main />
-      <Login />
     </>
   );
 }
