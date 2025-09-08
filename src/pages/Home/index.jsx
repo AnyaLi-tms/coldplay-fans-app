8fd58a206c9bfdab5d795281af6bf97955fa404b
Kayla Guo <guoka3@oocl.com>
2025-09-08 14:17:33 +0800

    feat: fix login and concert detail route

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,10 +1,17 @@
 import Banner from './Banner';
 import ConcertList from './ConcertList';
-
+import ConcertDetail from '../../pages/Home/ConcertDetail';
+import Login from '../../pages/Home/Login.jsx';
+import { Routes, Route } from 'react-router-dom';
 const Home = () => (
   <div>
     <Banner />
     <ConcertList />
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/concerts/:id" element={<ConcertDetail />} />
+      <Route path="/login" element={<Login />} />
+    </Routes>
   </div>
 );
 
