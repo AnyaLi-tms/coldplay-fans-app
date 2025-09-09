46627e5ab32373f7e9f8b190ee4dcf63bc7789ee
LIAN18 <lian18@oocl.com>
2025-09-09 09:04:50 +0800

    feat: format login

--- src/services/login.js
+++ src/services/login.js
@@ -5,16 +5,16 @@ export async function register(username, password) {
     const response = await api.post('user/register', { username, password });
     return response;
   } catch (err) {
-    return err?.response || { data: { success: false, message: err.message} };
+    return err?.response || { data: { success: false, message: err.message } };
   }
 }
 
 export async function login(username, password) {
   try {
     const response = await api.post('user/login', { username, password });
-    
+
     return response;
   } catch (err) {
-    return err?.response || { data: { success: false, message: err.message} };
+    return err?.response || { data: { success: false, message: err.message } };
   }
 }
