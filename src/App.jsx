4052ebc865eeb77f52c15f975e1c54c2a2617dec
OliverDong <oliver.dong@oocl.com>
2025-09-10 11:54:31 +0800

    feat:美化login页面

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
 import './App.css';
 import Header from './layout/Header';
 import Main from './layout/Main';
-
+import Login from './pages/Login';
+import { useLocation } from 'react-router-dom';
 function App() {
+  const location = useLocation();
+  if (location.pathname === '/login') {
+    return <Login />;
+  }
   return (
     <div className="App">
       <Header />
