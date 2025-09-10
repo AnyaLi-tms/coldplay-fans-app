a4ece43c2635435a678b70c0ad964ee94b8e4d9f
LIAN18 <lian18@oocl.com>
2025-09-10 13:38:51 +0800

    feat: update TabBar

--- src/layout/Header/TabBar/index.jsx
+++ src/layout/Header/TabBar/index.jsx
@@ -1,19 +1,29 @@
 import styles from './index.module.css';
 import { Menu } from 'antd';
+import { useNavigate, useLocation } from 'react-router-dom';
 
 const items = [
   { key: 'home', label: '主页', path: '/' },
   { key: 'merchandise', label: '周边', path: '/merchandise' },
 ];
 
-const TabBar = () => (
-  <Menu
-    className={styles.tabbar}
-    theme="dark"
-    mode="horizontal"
-    defaultSelectedKeys={['2']}
-    items={items}
-  />
-);
+const TabBar = () => {
+  const navigate = useNavigate();
+  const location = useLocation();
+  return (
+    <Menu
+      className={styles.tabbar}
+      theme="dark"
+      mode="horizontal"
+      selectedKeys={[
+        items.find((item) => item.path === location.pathname)?.key || 'home',
+      ]}
+      items={items.map((item) => ({
+        ...item,
+        onClick: () => navigate(item.path),
+      }))}
+    />
+  );
+};
 
 export default TabBar;
