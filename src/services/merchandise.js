99e175e4e17fc625f1160620bd7cc2b83fafc306
Kayla Guo <guoka3@oocl.com>
2025-09-11 09:19:03 +0800

    feat: change merchandise api

--- src/services/merchandise.js
+++ src/services/merchandise.js
@@ -1,14 +1,28 @@
 import api from './api';
 
 // 获取所有周边商品，支持前端名称模糊查询
-export const fetchMerchandiseList = (keyword = '') =>
-  api.get('/merchandises').then((res) => {
-    if (!keyword) return res.data;
-    return res.data.filter((item) => item.name.includes(keyword));
-  });
-
+// export const fetchMerchandiseList = (keyword = '') =>
+//    api.get('/merchandises').then((res) => {
+// //     if (!keyword) return res.data;
+// //     return res.data.filter((item) => item.name.includes(keyword));
+// //   });
+export const getAllConcerts = (params = {}) =>
+  api.get('/concerts', { params }).then((res) => res.data);
 export const loadMerchandiseOrders = () =>
   api.get('/myMerchandise').then((res) => res.data);
 
 export const buyMerchandise = async (data) =>
   api.put('/merchandises/buyMerchandise', data);
+
+// 获取所有周边商品，支持后端模糊查询
+export const fetchMerchandiseList = (query = '') => {
+  if (!query) {
+    // 获取全部商品
+    return api.get('/merchandises/distinct').then((res) => res.data);
+  } else {
+    // 后端模糊查询，参数名为 query
+    return api
+      .get('/merchandises/distinct', { params: { query } })
+      .then((res) => res.data);
+  }
+};
--- src/store/merchandiseStore.js
+++ src/store/merchandiseStore.js
@@ -1,9 +1,5 @@
 import { create } from 'zustand';
-import {
-  fetchMerchandiseList,
-  loadMerchandiseOrders,
-  buyMerchandise,
-} from '../services/merchandise';
+import { fetchMerchandiseList } from '../services/merchandise';
 
 export const useMerchandiseStore = create((set, get) => ({
   merchandiseOrders: [],
@@ -11,32 +7,11 @@ export const useMerchandiseStore = create((set, get) => ({
   loading: false,
   error: null,
 
-  fetchMerchandise: async (keyword = '') => {
+  fetchMerchandise: async (query = '') => {
     set({ loading: true, error: null });
     try {
-      const list = await fetchMerchandiseList(keyword);
+      const list = await fetchMerchandiseList(query);
       set({ merchandise: list, loading: false });
-    } catch (err) {
-      set({ error: err, loading: false });
-    }
-  },
-
-  fetchBuyMerchandise: async (data) => {
-    set({ loading: true, error: null });
-    try {
-      const res = await buyMerchandise(data);
-      set({ merchandiseOrders: res.data, loading: false });
-    } catch (err) {
-      set({ error: err.response?.data || err.message, loading: false });
-      return err.response?.data || { status: 'false', msg: '购买周边失败' };
-    }
-  },
-
-  loadMerchandiseOrders: async () => {
-    set({ loading: true, error: null });
-    try {
-      const res = await loadMerchandiseOrders();
-      set({ merchandiseOrders: res.data, loading: false });
     } catch (err) {
       set({ error: err, loading: false });
     }
